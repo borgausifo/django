48af257a44dc8c1058b5953dada087307f19171c
Borga Usifo <borga.usifo@gmail.com>
2020-02-01 16:57:18 -0600

    Adding password urls and views

--- django-apps/blogpost_web_app/django_project/blog/views.py
+++ django-apps/blogpost_web_app/django_project/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView  # --> class based views
 from .models import Post
 
@@ -17,7 +18,20 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
+    # ordering = ['-date_posted'] --> This is not needed because we are querying on userpostlistview
+    paginate_by = 5  # --> For pagination no need to import class based views
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5  # --> For pagination no need to import class based views
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
